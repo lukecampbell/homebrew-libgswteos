fe0f34682e4790b00bf4f3d5b0d659475e093364
Luke Campbell <luke.s.campbell@gmail.com>
2013-03-11 10:56:16 -0400

    Updates to release version 4
    r4 on libgswteos is tested on linux, there was a missing -lm flag for
    compiling the check functions.

--- Formula/libgswteos-10.rb
+++ Formula/libgswteos-10.rb
@@ -1,12 +1,9 @@
 require 'formula'
-#
-# TEOS-10 V3.0 GSW Oceanographic Toolbox in C
-#
 class Libgswteos10 < Formula
-  url 'https://github.com/lukecampbell/gsw-teos/tarball/v3.0r3'
+  url 'https://github.com/lukecampbell/gsw-teos/tarball/v3.0r4'
   homepage 'http://www.teos-10.org/'
   version '3.0'
-  sha1 'aa58bdfbe92d40504713cd9b8ed39f59c8d04627'
+  sha1 '6ae190b7da78d6aff7859e7d1a3bb027ce6cc5f3'
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
@@ -15,7 +12,7 @@ class Libgswteos10 < Formula
     system "./configure", "--disable-debug", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
     system "make"
-    system "make install" # if this fails, try separate make/make install steps
+    system "make install" 
   end
   def test
     system "#{bin}/gsw_check_functions"
