d234bfa93fe0092e707e300266f30fb3fa5d0770
Peter Cable <petercable@gmail.com>
2017-04-26 16:27:24 -0700

    Updated to use sha256

--- Formula/libgswteos-10.rb
+++ Formula/libgswteos-10.rb
@@ -3,7 +3,7 @@ class Libgswteos10 < Formula
   url 'https://github.com/lukecampbell/gsw-teos/tarball/v3.0r4'
   homepage 'http://www.teos-10.org/'
   version '3.0'
-  sha1 '6ae190b7da78d6aff7859e7d1a3bb027ce6cc5f3'
+  sha256 '0e0b902dc90a1c38cecc84090618a61be538df876fd7b10b92776574d7223edd'
   depends_on 'autoconf' => :build
   depends_on 'automake' => :build
   depends_on 'libtool' => :build
